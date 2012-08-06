450d7f8f1c7b2e5158775648319f08b84acb2895
Željko Filipin <zeljko.filipin@gmail.com>
2012-08-06 23:07:14 +0200

    cleanup: whitespace

--- lib/pages/proofread_page.rb
+++ lib/pages/proofread_page.rb
@@ -14,7 +14,7 @@ class ProofreadPage
   span(:link_page, :title => 'Link')
   span(:reference, :title => 'Reference')
   image(:ocr, :id => 'wsOcr1')
-  
+
   #collapsible links display additional features
   link(:advanced, :text => 'Advanced')
   link(:level2, :text => 'Level 2')
@@ -33,7 +33,7 @@ class ProofreadPage
   span(:gallery, :title => 'Picture gallery')
   span(:redirect, :title => 'Redirect')
   span(:table, :title => 'Table')
-  
+
   #define the first 3 char sets and the first char in each
   link(:special, :text => 'Special characters')
   div(:latin, :text => 'Latin')
@@ -42,20 +42,17 @@ class ProofreadPage
   span(:latin_extended_a, :text => 'Ḁ')
   div(:ipa, :text => 'IPA')
   span(:ipa_p, :text => 'p')
-  
+
   #define the first 3 help links
   link(:help, :text => 'Help')
   link(:formatting, :text => 'Formatting')
   link(:links_help, :text => 'Links')
   link(:headings, :text => 'Headings')
-  
+
   link(:proofread, :text => 'Proofread tools')
   image(:zoom_in, :title => 'Zoom in')
   image(:zoom_out, :title => 'Zoom out')
   image(:original_size, :title => 'Original size')
   #image(:show_hide, :title, => "Show/hide this page's header and footer")
   image(:vertical_horizontal, :title => 'Vertical/horizontal layout')
-  
 end
-
-
