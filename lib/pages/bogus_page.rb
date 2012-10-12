c2fc02283478a30bc112b7040a3c2e8e35aeb394
Zeljko Filipin <zeljko.filipin@gmail.com>
2012-10-12 16:47:16 +0200

    refactoring: using page-object api that is more similar to watir api
    :text instead of :link_text

--- lib/pages/bogus_page.rb
+++ lib/pages/bogus_page.rb
@@ -4,6 +4,6 @@ class BogusPage
   #http://en.wikipedia.beta.wmflabs.org/wiki/Bogus_page
   page_url 'https://en.wikipedia.org/wiki/Bogus_page'
 
-  a(:search, :link_text => 'search for Bogus page in Wikipedia')
-  a(:search2, :link_text => 'Search for "Bogus page"')
+  a(:search, :text => 'search for Bogus page in Wikipedia')
+  a(:search2, :text => 'Search for "Bogus page"')
 end
