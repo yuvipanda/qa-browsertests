9a4197f96e9d480842a31050b0a9f820fb3b5219
Zeljko Filipin <zeljko.filipin@gmail.com>
2012-10-16 16:42:44 +0200

    delete "search for string" spec
    because the link does not exist in MediaWiki 1.21wmf2

--- lib/pages/bogus_page.rb
+++ lib/pages/bogus_page.rb
@@ -5,5 +5,4 @@ class BogusPage
   page_url "#{config['base_url']}Bogus_page"
 
   a(:search_for_page, :text => 'search for Bogus page in Wikipedia')
-  a(:search_for_string, :text => 'Search for "Bogus page"')
 end
