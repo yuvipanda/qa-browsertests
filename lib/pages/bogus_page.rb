a5eb69f747ff9f7a5cdbfb435209c3c7cd848a87
Zeljko Filipin <zeljko.filipin@gmail.com>
2012-10-16 19:21:07 +0200

    refactoring: introduce base_url top level configuration option
    preparing for introducing more top level configuration options

--- lib/pages/bogus_page.rb
+++ lib/pages/bogus_page.rb
@@ -2,7 +2,7 @@ class BogusPage
   include PageObject
 
   config = YAML.load_file('config/config.yml')
-  page_url "#{config['base_url']}Bogus_page"
+  page_url "#{config['base_url']['test2']}Bogus_page"
 
   a(:search_for_page, :text => 'search for this page title')
 end
