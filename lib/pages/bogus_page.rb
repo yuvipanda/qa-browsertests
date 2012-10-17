3986e098b7cf04e61ba108f6602db580445ea24a
Zeljko Filipin <zeljko.filipin@gmail.com>
2012-10-17 16:35:09 +0200

    detect browser and use desktop or mobile site

--- lib/pages/bogus_page.rb
+++ lib/pages/bogus_page.rb
@@ -1,8 +1,14 @@
 class BogusPage
   include PageObject
 
+  if ENV['BROWSER_NAME'] == "android" or  ENV['BROWSER_NAME'] == "iphone"
+    site = "testm"
+  else
+    site = "test2"
+  end
+
   config = YAML.load_file('config/config.yml')
-  page_url "#{config['base_url']['test']}Bogus_page"
+  page_url "#{config['base_url'][site]}Bogus_page"
 
   a(:search_for_page, :text => 'search for this page title')
 end
