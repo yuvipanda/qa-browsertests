be70db5d538d16603b36edf46ff533ec10190bc6
Zeljko Filipin <zeljko.filipin@gmail.com>
2012-11-26 20:46:07 +0100

    Workaround for Crome refusing to upload files
    The workaround forces file upload element to appear on the page by
    removing it's class any style attributes. We should really find a
    better way to do this, but it works for now.
    
    Change-Id: I8841929ab2ba46e131f715f431902a8f978ba974

--- features/step_definitions/uploadwizard_steps.rb
+++ features/step_definitions/uploadwizard_steps.rb
@@ -37,6 +37,10 @@ When /^upload file (.+)$/ do |file_name|
   require "chunky_png"
   ChunkyPNG::Image.new(Random.new.rand(255), Random.new.rand(255), Random.new.rand(255)).save file_name
   path = Dir.pwd + "/" + file_name
+  if @browser.driver.browser == :chrome
+    @browser.execute_script 'document.getElementsByName("file")[0].removeAttribute("class");'
+    @browser.execute_script 'document.getElementsByName("file")[0].removeAttribute("style");'
+  end
   on(UploadPage).select_file = path
 end
 
