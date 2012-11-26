60b3e76a9d72da84fefba1ab0f44e37b36c9febc
Zeljko Filipin <zeljko.filipin@gmail.com>
2012-11-26 23:46:51 +0100

    Sorting page elements by aphabet.
    Makes it easier to find them. Also makes it easier to find name
    conflicts.
    
    Change-Id: I3da342618898349197e3faedb61d7f6b775c826f

--- features/support/pages/aftv5_page.rb
+++ features/support/pages/aftv5_page.rb
@@ -1,5 +1,6 @@
 class AftV5Page
   include PageObject
+  
   #THIS TEST SHOULD RUN ONLY ON BETA LABS UNTIL AFTV5 IS 100% ENABLED ON TEST2/PROD
   def self.url
     base_url = "wmflabs"
@@ -8,21 +9,22 @@ class AftV5Page
     "#{config['base_url'][base_url]}Aftpage"
   end
   page_url url
+  
   div(:aft_box, :id => 'mw-articlefeedbackv5')
-  link(:float_tip, :link_text => "What's this?")
-  span(:yes_span, :text => "Yes")
-  span(:no_span, :text => "No")
+  link(:all_comments, :link_text => /All comments/)
   link(:back_to_yesno, :link_text => "Back to step 1")
-  link(:terms, :href => '//wikimediafoundation.org/wiki/Feedback_privacy_statement', :link_text => 'terms')
+  span(:create_account, :text => 'Create an account')
+  link(:feedback_page, :link_text => 'feedback page')
+  link(:float_tip, :link_text => "What's this?")
   link(:helpful_feedback, :href => 'http://en.wikipedia.org/wiki/Wikipedia:Feedback_guidelines', :link_text => 'helpful feedback')
   text_area(:input_area, :id => 'articleFeedbackv5-find-feedback')
-  span(:post_feedback, :text => 'Post your feedback')
-  link(:feedback_page, :link_text => 'feedback page')
-  span(:create_account, :text => 'Create an account')
-  link(:log_in, :link_text => 'Log in')
-  link(:all_comments, :link_text => /All comments/)
-  link(:most_relevant, :link_text => /Most relevant/)
-  select_list(:sort_by, :id => 'articleFeedbackv5-sort-select')
-  link(:view_article, :link_text => 'View article')
   link(:learn_more, :link_text => /Learn more/)
+  link(:log_in, :link_text => 'Log in')
+  link(:most_relevant, :link_text => /Most relevant/)
+  span(:no_span, :text => "No")
+  span(:post_feedback, :text => 'Post your feedback')
+  select_list(:sort_by, :id => 'articleFeedbackv5-sort-select')
+  link(:terms, :href => '//wikimediafoundation.org/wiki/Feedback_privacy_statement', :link_text => 'terms')
+  link(:view_article, :link_text => 'View article')
+  span(:yes_span, :text => "Yes")
 end
