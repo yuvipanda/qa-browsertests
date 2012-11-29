ddfeba54d72581c22107e722776ea4cdfb5ca4b0
Zeljko Filipin <zeljko.filipin@gmail.com>
2012-11-29 11:21:38 +0100

    Skip mobile search test for IE 6 and 7
    Change-Id: I9b63f72e523ca0d6ee480243832d5e26620d0723

--- features/step_definitions/mobilesearch_steps.rb
+++ features/step_definitions/mobilesearch_steps.rb
@@ -13,7 +13,7 @@ end
 
 Then /^Search results matching my characters appear$/ do
   # http://www.mediawiki.org/wiki/Mobile/Testing_process#SmartPhone
-  pending if ENV['BROWSER_LABEL'] == "internet_explorer_6"
+  pending if ENV['BROWSER_LABEL'].match /internet_explorer_[67]/
   on(MobilePage) do |page|
     page.wait_until(10) do
       page.text.include? "San Francisco"
