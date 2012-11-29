b6db57285d8df1927b0840a9a96458b921d588fc
Zeljko Filipin <zeljko.filipin@gmail.com>
2012-11-29 16:11:06 +0100

    Marking test pending because of known bug 42517
    in IE link to feedback page requires two clicks to navigate
    https://bugzilla.wikimedia.org/show_bug.cgi?id=42517
    
    Change-Id: I4e37d30dd136ae1cf4697d36ded1728176123d77

--- features/step_definitions/aftv5_steps.rb
+++ features/step_definitions/aftv5_steps.rb
@@ -82,6 +82,9 @@ Then /^I see helpful feedback guide and terms$/ do
   end
 end
 Then /^When I click to navigate to comments page my saved comment appears$/ do
+  # https://bugzilla.wikimedia.org/show_bug.cgi?id=42517
+  pending if ENV['BROWSER_LABEL'] == "internet_explorer_7"
+
   on(AFTv5Page) do |page|
     page.all_comments_element.when_present.click
     page.wait_until(10) do
