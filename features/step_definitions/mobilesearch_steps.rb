05ca32aaf66baac5ffaba2e568b5125a3439122f
Zeljko Filipin <zeljko.filipin@gmail.com>
2012-12-10 17:10:54 +0100

    Cleanup: deleted commented out code
    Change-Id: I381e21ea4b326cc987945555d34a43c3b49d09fe

--- features/step_definitions/mobilesearch_steps.rb
+++ features/step_definitions/mobilesearch_steps.rb
@@ -20,21 +20,3 @@ Then /^Search results matching my characters appear$/ do
     end
   end
 end
-
-#WATCHLIST LINK APPEARS FOR CHROME BUT NOT FOR FIREFOX, BUGZILLA TICKET TBD
-
-#And /^There is a a watchlist link on the page$/ do
-#  on(MobilePage) do |page|
-#    page.watch_link_element.should be_true
-#  end
-#end
-
-#And /^When the watchlist link is clicked it becomes watched$/ do
-#  on(MobilePage) do |arg|
-#    @page = arg
-#    @page.watch_link
-#    @page.wait_until(10) do
-#      @page.watched_link_element.exists?
-#    end
-#  end
-#end
