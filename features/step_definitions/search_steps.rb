761f171a5314d1c3fe2ea67257807622587e421e
Zeljko Filipin <zeljko.filipin@gmail.com>
2012-12-11 17:37:04 +0100

    Refactoring: moving code, nothing changed
    Actually, one "and" step got renamed to "when", but that is really
    minor change.
    
    Change-Id: I77f20b2d8f9bfe5e369662c1d863defe007f048b

--- features/step_definitions/search_steps.rb
+++ features/step_definitions/search_steps.rb
@@ -2,6 +2,9 @@ Given /^I am at random page$/ do
   visit RandomPage
 end
 
+When /^I click the Search button$/ do
+  on(SearchPage).search_button
+end
 When /^I search for: (.+)$/ do |search_term|
   on(SearchPage).search_input= search_term
 end
@@ -9,15 +12,9 @@ end
 Then /^a list of suggested pages should appear$/ do
   on(SearchPage).search_results_element.when_present.should exist
 end
-
-Then /^(.+) should be the first result$/ do |page_name|
-  on(SearchPage).one_result.should == page_name
-end
-
-And /^I click the Search button$/ do
-  on(SearchPage).search_button
-end
-
 Then /^I should land on Search Results page$/ do
   @browser.url.should match '&title=Special%3ASearch$'
+end
+Then /^(.+) should be the first result$/ do |page_name|
+  on(SearchPage).one_result.should == page_name
 end
