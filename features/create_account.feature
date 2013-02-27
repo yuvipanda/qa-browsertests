fd95cd3a6ae92d7678216a942316647303fe7602
S Page <spage@wikimedia.org>
2013-02-27 15:19:55 -0800

    Add proper test for ResourceLoader errors.
    The test is only for create_account; it should be promoted to PageObject
    so that visiting any MediaWiki page checks for RL errors.
    
    Bug: 44299
    Change-Id: Idaadb99855f0462b2fe50d18b7221235c3bce3dc

--- features/create_account.feature
+++ features/create_account.feature
@@ -3,6 +3,7 @@ Feature: Create account
   Scenario Outline: Go to Create account page
     Given I go to Create account page at <path>
     Then form has Create account button
+      And page has no ResourceLoader errors
 
   Examples:
     | path |
--- features/step_definitions/create_account_steps.rb
+++ features/step_definitions/create_account_steps.rb
@@ -5,3 +5,38 @@ end
 Then /^form has Create account button$/ do
   on(CreateAccountPage).create_account_element.should exist
 end
+
+Then /^page has no ResourceLoader errors$/ do
+  @browser.execute_script("
+return (function() {
+    // Returns a string listing problem modules,
+    // or empty string if all OK (or not a MediaWiki page).
+    // MediaWiki registers many JS modules that are not loaded on average pages.
+    var i, len, state,
+        modules = mw.loader.getModuleNames(),
+        error = [],
+        missing = [],
+        ret = '';
+    if ( ( typeof mediaWiki === 'undefined' ) || !mediaWiki.loader ) {
+        return ret;
+    }
+
+    for ( i = 0, len = modules.length; i < len; i++ ) {
+        state = mw.loader.getState( modules[i] );
+        if ( state === 'error' ) {
+            error.push( modules[i] );
+        }
+        if ( state === 'missing' ) {
+            missing.push( modules[i] );
+        }
+    }
+    if ( error.length ) {
+       ret += 'Error modules: ' + error.join( '; ') + '.';
+    }
+    if ( missing.length ) {
+       ret += 'Missing modules: ' + missing.join( '; ') + '.';
+    }
+    return ret;
+}) ();
+").should == ''
+end
