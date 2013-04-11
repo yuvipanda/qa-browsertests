5ce392b619b17ec25699c05dece58db50f557ce4
Zeljko Filipin <zeljko.filipin@gmail.com>
2013-04-11 18:43:18 +0200

    Deleted unnecessary page
    Reused existing page.
    
    Change-Id: Ia43f8e7af626a66f8e083978f18c13bdcf54b23d

--- features/step_definitions/universal_language_selector_steps.rb
+++ features/step_definitions/universal_language_selector_steps.rb
@@ -1,11 +1,7 @@
-Given /^I am at random translate wiki page$/ do
-  visit(RandomTranslateWikiPage)
-end
-
 When /^I click language selector trigger element$/ do
-  on(RandomTranslateWikiPage).uls_trigger
+  on(RandomPage).uls_trigger
 end
 
 Then /^I should see the Language selector$/ do
-  on(RandomTranslateWikiPage).search_element.should exist
+  on(RandomPage).search_element.should exist
 end
--- features/support/pages/random_page.rb
+++ features/support/pages/random_page.rb
@@ -1,9 +1,17 @@
 class RandomPage
   include PageObject
-  include URLModule
 
   def self.url
-    URLModule.url('Special:Random')
+    if ENV['MEDIAWIKI_URL']
+      mediawiki_url = ENV['MEDIAWIKI_URL']
+    else
+      mediawiki_url = 'http://en.wikipedia.beta.wmflabs.org/wiki/'
+    end
+    "#{mediawiki_url}Special:Random"
   end
   page_url url
+
+  li(:main_page, id: 'n-mainpage-description')
+  a(:uls_trigger, class: 'uls-trigger')
+  div(:search, id: 'search')
 end
